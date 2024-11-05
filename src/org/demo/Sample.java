c3186a89ff91bb9c7841e346a421b939c6198372
Devaaaga <Devaaaga>
2024-11-05 13:58:38 +0530

    done

---
+++ src/org/demo/Sample.java
@@ -0,0 +1,12 @@
+package org.demo;
+
+public class Sample {
+private void method1() {
+System.out.println("Method 1");
+}
+public static void main(String[] args) {
+	
+	Sample s = new Sample();
+	s.method1();
+}
+}
