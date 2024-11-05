6100ff4e719220ed807bced97b4ab5adfcc08679
Devaaaga <Devaaaga>
2024-11-05 14:07:15 +0530

    done

--- src/org/demo/Sample.java
+++ src/org/demo/Sample.java
@@ -4,9 +4,13 @@ public class Sample {
 private void method1() {
 System.out.println("Method 1");
 }
+private void methiod2() {
+System.out.println("Methiod 2");
+}
 public static void main(String[] args) {
 	
 	Sample s = new Sample();
 	s.method1();
+	s.methiod2();
 }
 }
